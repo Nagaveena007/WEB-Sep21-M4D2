90de0d9d44ed8893ec3c5979e938646e6f4407fc
Nagaveena007 <nagaveena.hallikeri@gmail.com>
2021-11-09 19:12:28 +0100

    done with Tapestry

--- strive-books/src/Components/Tapestry.jsx
+++ strive-books/src/Components/Tapestry.jsx
@@ -1,10 +1,16 @@
-import { Book, Container, Row } from "react-bootstrap";
+import { Card, Container, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import items from "..Components/scifi.json";
+import books from "../data/scifi.json";
 
 const TapestryThing = () => (
   <Container>
-    <Row></Row>
+    <Row>
+      <Col xs={12} md={3} lg={2} className="text-center">
+        {books.map((bookObject) => (
+          <Card.Img key={bookObject.asin} variant="top" src={bookObject.img} />
+        ))}
+      </Col>
+    </Row>
   </Container>
 );
 export default TapestryThing;
